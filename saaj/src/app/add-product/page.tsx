b561d92f0e07c8ff433f0fd8006f5e5721418bee
noxayyyy <aisuru.nox@gmail.com>
2025-07-17 09:30:13 +0500

    slight tuning

--- saaj/src/app/add-product/page.tsx
+++ saaj/src/app/add-product/page.tsx
@@ -18,47 +18,38 @@ async function addProduct(form_data: FormData) {
 		redirect("/api/auth/signin?callbackUrl=/add-product");
 	}
 
-	let name = form_data.get("name")?.toString();
+	const name = form_data.get("name")?.toString();
 	const sku = form_data.get("sku")?.toString();
 	const desc = form_data.get("desc")?.toString();
 	const image_urls = form_data.getAll("image_url").map(url => url.toString());
-	let cost = Number(form_data.get("cost"));
+	const cost = Number(form_data.get("cost"));
 	const currency_rates = await prisma.currency.findMany({});
 
 	if (!name || !sku || !desc || !image_urls[0] || !cost) {
 		throw Error("Missing required fields");
 	}
 
-	for (let i = 0; i < 50; i++) {
-		name = `${name} ${i}`;
-		cost = Math.random() * 500;
-		for (let i = image_urls.length - 1; i > 0; i--) {
-			const j = Math.floor(Math.random() * (i + 1));
-			[image_urls[i], image_urls[j]] = [image_urls[j], image_urls[i]];
-		}
-		await prisma.product.create({
-			data: {
-				name,
-				sku,
-				desc,
-				image_urls,
-				def_price: cost,
-				prices: {
-					create: supported_countries.map((country) => {
-						return {
-							currency: countryToCurrency[country],
-							amount: Math.ceil(
-								(cost * currency_rates.filter(
-									(rate) => rate.name === countryToCurrency[country]
-								)[0].rate) / (country === "PK" ? 100 : 10)
-							) * (country === "PK" ? 100 : 10)
-						}
-					})
-				}
-			},
-		});
-		name = name.split(" ")[0];
-	}
+	await prisma.product.create({
+		data: {
+			name,
+			sku,
+			desc,
+			image_urls,
+			def_price: cost,
+			prices: {
+				create: supported_countries.map((country) => {
+					return {
+						currency: countryToCurrency[country],
+						amount: Math.ceil(
+							(cost * currency_rates.filter(
+								(rate) => rate.name === countryToCurrency[country]
+							)[0].rate) / (country === "PK" ? 100 : 10)
+						) * (country === "PK" ? 100 : 10)
+					}
+				})
+			}
+		},
+	});
 
 	// redirect("/");
 }
